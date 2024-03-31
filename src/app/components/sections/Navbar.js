0161fbecd4f6b8c476a6cc193e71d88b4e124198
Taher <itscross123@gmail.com>
2024-03-31 14:58:34 +0530

    Made some UI improvements in Navbar

--- src/app/components/sections/Navbar.js
+++ src/app/components/sections/Navbar.js
@@ -7,17 +7,17 @@ function Navbar({ scrollToRef, servicesRef }) {
   return (
     <nav className="flex items-center justify-center w-full text-white">
       <div className="px-20 py-4  bg-white/30  flex gap-20 items-center justify-center mt-4 rounded-xl">
-        <Link href="/" className="text-xl GilroyBold">
+        <Link href="/" className="text-lg font-semibold">
           LOGO
         </Link>
         <Link
           href="/"
-          className="text-xl GilroyBold"
+          className="text-lg font-semibold"
           onClick={() => scrollToRef(servicesRef)}
         >
           Services
         </Link>
-        <Link href="/about" className="text-xl GilroyBold">
+        <Link href="/about" className="text-lg font-semibold">
           About
         </Link>
       </div>
