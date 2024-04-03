b441b5468a3cf32b6b29dfc60444c8ce5817fc2a
Taher <itscross123@gmail.com>
2024-04-03 23:47:12 +0530

    Fixed navigation issue in mobile

--- src/app/components/sections/Navbar.js
+++ src/app/components/sections/Navbar.js
@@ -7,7 +7,7 @@ import React from "react";
 function Navbar({ scrollToRef, servicesRef }) {
   return (
     <nav className="flex items-center justify-center md:w-full text-[#1e40af]">
-      <div className="md:px-20 px-10 py-1 bg-gray-200 flex md:gap-20 gap-8 items-center justify-center mt-4 rounded-xl">
+      <div className="md:px-20 px-10 py-1 bg-gray-200 flex md:gap-20 gap-8 items-center justify-center mt-4 rounded-xl z-50">
         <Link href="/" className="md:text-lg text-base font-semibold">
           <Image src={"/logo.png"} width={70} height={70} alt="DokLink Logo" />
         </Link>
